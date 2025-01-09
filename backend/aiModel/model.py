68d48847cca1cc94ef1a4617f764563524f80bad
MrSamerM <2106040@brunel.ac.uk>
2025-01-09 21:36:24 +0000

    added new images and using python, installed all suitbalelibraries

---
+++ backend/aiModel/model.py
@@ -0,0 +1,18 @@
+# // For some images in NotContainer I used images from kaggle using data from the dataset from:
+# // https://www.kaggle.com/datasets/utkarshsaxenadn/landscape-recognition-image-dataset-12k-images/data Use kaggle for scenary
+# // https://www.kaggle.com/datasets/vikashrajluhaniwal/fashion-images Use kaggle for clothes and shoes
+# // https://www.kaggle.com/datasets/vencerlanz09/plastic-and-paper-cups-synthetic-image-dataset use kaggle for cups
+# // https://www.kaggle.com/datasets/boulahchichenadir/algerian-used-cars use kaggle for vehicles
+
+
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL 
+
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.models import Sequential
+
+print("TensorFlow and Keras imports successful!")
+
