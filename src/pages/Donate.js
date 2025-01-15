6a2e947f94b8306a7292df87480e1dc431926b49
MrSamerM <2106040@brunel.ac.uk>
2025-01-15 21:51:10 +0000

    Started the donation

--- src/pages/Donate.js
+++ src/pages/Donate.js
@@ -1,12 +1,44 @@
 import React from "react";
 import '../styling/Donate.css'
+import { useState, useEffect } from "react";
 
+// https://www.geeksforgeeks.org/how-to-upload-image-and-preview-it-using-reactjs/ do display image 15/01/25
 
 function Donate() {
+
+    const [file, setFile] = useState("");
+    const [enableInput, setEnableInput] = useState(false);
+    const [address, setAddress] = useState("");
+
+    useEffect(() => {
+        if (file[0].endsWith('.png') || file[0].endsWith('.jpg') || file[0].endsWith('.gif')) {
+            setEnableInput(true);
+            console.log("true");
+            console.log(file);
+        }
+        else {
+            setEnableInput(false);
+            console.log("false");
+            console.log(file);
+
+        }
+    }, []);
+
     return (
-        <div>
+        <>
             <h1>Donate</h1>
-        </div>
+
+            <div>
+                <input type="file" id="imageFile" onChange={(evt) => setFile(URL.createObjectURL(evt.target.files[0]))} />
+                <img src={file} />
+            </div>
+            {enableInput === false ? null :
+                <div>
+                    <input type="text" id="address" value={address} onChange={(evt) => setAddress(evt.target.value)} />
+                </div>
+            }
+
+        </>
     );
 }
 
