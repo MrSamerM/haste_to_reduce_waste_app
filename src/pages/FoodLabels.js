d66cacd3a67c5f1606fabe6f40565c52eef9a9cd
MrSamerM <2106040@brunel.ac.uk>
2025-02-01 22:06:21 +0000

    I still need to fix the function

--- src/pages/FoodLabels.js
+++ src/pages/FoodLabels.js
@@ -45,15 +45,72 @@ function FoodLabels() {
 
 
     // https://stackoverflow.com/questions/7335075/compare-2-dates-in-format-dd-mm-yyyy-with-javascript-jquery By Maxx 31/01/2025
+    // https://sentry.io/answers/how-to-insert-an-item-into-an-array-at-a-specific-index-using-javascript/ 01/02/2025 for .splice method
 
     function dateConvertion(date) {
-        const parts = date.split("/");
-        // this changes date to mm/dd/yy
-        const newDate = new Date(parts[1] + "/" + parts[0] + "/" + parts[2]);
-        return newDate.getTime();
+
+        if (date.match(/\d{2} [A-Za-z]{3} \d{2,4}/)) {
+            const parts = date.split(" ");
+
+            if (parts[2].length === 2) {
+                parts[2] = `20${parts[2]}`;
+            }
+
+            if (parts[1].toUpperCase() === "JAN") {
+                parts[1] = 0
+            }
+            else if (parts[1].toUpperCase() === "FEB") {
+                parts[1] = 1
+            }
+            else if (parts[1].toUpperCase() === "MAR") {
+                parts[1] = 2
+            }
+            else if (parts[1].toUpperCase() === "APR") {
+                parts[1] = 3
+            }
+            else if (parts[1].toUpperCase() === "MAY") {
+                parts[1] = 4
+            }
+            else if (parts[1].toUpperCase() === "JUN") {
+                parts[1] = 5
+            }
+            else if (parts[1].toUpperCase() === "JUL") {
+                parts[1] = 6
+            }
+            else if (parts[1].toUpperCase() === "AUG") {
+                parts[1] = 7
+            }
+            else if (parts[1].toUpperCase() === "SEP") {
+                parts[1] = 8
+            }
+            else if (parts[1].toUpperCase() === "OCT") {
+                parts[1] = 9
+            }
+            else if (parts[1].toUpperCase() === "NOV") {
+                parts[1] = 10
+            }
+            else if (parts[1].toUpperCase() === "DEC") {
+                parts[1] = 11
+            }
+            const newDate = new Date(parts[1] + "/" + parts[0] + "/" + parts[2])
+            return newDate.getTime();
+        }
+
+        else if (date.match(/\d{2}\/\d{2}\/\d{2,4}/)) {
+            const parts = date.split("/");
+            // this changes date to mm/dd/yy
+            const newDate = new Date(parts[1] + "/" + parts[0] + "/" + parts[2]);
+            return newDate.getTime();
+        }
+
+        else if (date.match(/\d{2}\-\d{2}\-\d{2,4}/)) {
+            const parts = date.split("-");
+            // this changes date to mm/dd/yy
+            const newDate = new Date(parts[2], parts[0] - 1, parts[1]);
+            return newDate.getTime();
+        }
+
     }
-
-
     const fileInputRef = useRef(null);
 
     const submit = async (event) => {
@@ -71,52 +128,58 @@ function FoodLabels() {
 
             // https://stackoverflow.com/questions/65461724/how-can-i-remove-commas-or-whatever-from-within-a-string answered by munerik 30/01/2025
 
-            const result = response.data.text[0];
+            const result = response.data.text;
             console.log(result);
-            const newString = result.toString().replace(/[!.,]/g, ' ')
+            // const newString = result.toString().replace(/[!.,]/g, ' ')
 
-            const pattern = /(BB|Expiry Date|BBE|EXP|BEST BY|Best By|Best Before|\d{1,2}\/\d{1,2}\/\d{2,4}|\d{6}|\d{1,2}\-\d{1,2}\-\d{2,4}|\d{1,2}\.\d{1,2}\.\d{2,4}|\d{1,2}\ \d{1,2}\ \d{2,4}|\d{1,2}[a-zA-Z]{3}\d{2,4})/gim;
+            // const pattern = /(BB|Expiry Date|BBE|EXP|BEST BY|Best By|Best Before|\d{1,2}\/\d{1,2}\/\d{2,4}|\d{6}|\d{1,2}\-\d{1,2}\-\d{2,4}|\d{1,2}\.\d{1,2}\.\d{2,4}|\d{1,2} \d{1,2} \d{2,4}|\d{1,2}[a-zA-Z]{3}\\d{1,2} [a-zA-Z]{3} \d{2,4})/i;
 
-            const matches = [...newString.matchAll(pattern)];
-            const output = matches.map(match => match[0]).join(" ");
-            console.log(output);
-            const workArray = output.split(" ");
-            setText(output);
-            console.log(workArray);
+            // const matches = [...newString.matchAll(pattern)];
+            // const output = matches.map(match => match[0]).join(" ");
+            // console.log(output);
+            // const workArray = output.split(" ");
+            // setText(output);
+            // console.log(workArray);
 
-            const array = ["best before", "Expiry date", "10/09/2024", "10/10/2024"];
+            // const array = ["best before", "Expiry date", "10/09/2024", "10/10/2024"];
             const numberArray = [];
 
             const pattern1 = /(BB|Expiry Date|BBE|EXP|BEST BY|Best By|Best Before)/i;
-            const pattern2 = /(\d{1,2}\/\d{1,2}\/\d{2,4})/i;
+            const pattern2 = /(\d{1,2}\/\d{1,2}\/\d{2,4}|\d{6}|\d{1,2}\-\d{1,2}\-\d{2,4}|\d{1,2}\.\d{1,2}\.\d{2,4}|\d{1,2} \d{1,2} \d{2,4}|\d{1,2}[A-Za-z]{3}\d{1,2}|\d{1,2} [A-Za-z]{3} \d{2,4})/i;
 
             // filter method recieved by chatgpt, and remove gm, due to global issues with .test()
             //prompt: I have a array but I want to remove the elements that dont follow a regex pattern. 31/01/2025
             //prompt2: why is this not working (the method).
 
-            const words = array.filter(element => pattern1.test(element));
-            const dates = array.filter(element => pattern2.test(element));
+            const words = result.filter(element => pattern1.test(element));
+            const dates = result.filter(element => pattern2.test(element));
 
-            console.log(words);
-            console.log(dates);
+            console.log("this is word", words);
+            console.log("this is dates", dates);
 
             for (let i = 0; i < words.length; i++) {
                 if (words[i].toUpperCase().charAt(0) === "B") {
                     numberArray.push({
-                        word: array[i],
+                        word: words[i],
                         number: 1
                     });
                 }
                 else if (words[i].toUpperCase().charAt(0) === "E") {
                     numberArray.push({
-                        word: array[i],
+                        word: words[i],
                         number: 3
                     });
                 }
 
             }
 
-            if (dateConvertion(dates[0]) < dateConvertion(dates[1])) {
+            if (dates.length === 1) {
+                numberArray.push({
+                    word: dates[0],
+                    number: 2
+                });
+            }
+            else if (dateConvertion(dates[0]) < dateConvertion(dates[1])) {
                 numberArray.push({
                     word: dates[1],
                     number: 4
@@ -170,16 +233,18 @@ function FoodLabels() {
             // Use by date is the date where the food consumed has to be eaten before or on that date.
             // Expiry date is similar to use by date. Meaning it is not recommended to be conumed after the date. 
 
-            if ((numberArray[0].word.toUpperCase().charAt(0) === ("B" || "D")) && (numberArray[2].word.toUpperCase().charAt(0) === ("E") || ("U"))) {
-                console.log(`This means that you have until ${numberArray[1].word} to consume while it is closed, however if stored properly, and quality looks suitbale, it make extend over the date`)
-                console.log(`This means that you have until ${numberArray[3].word} to consume. That is it`)
-            }
-            else if ((numberArray[0].word.toUpperCase().charAt(0) === ("E") || ("U"))) {
+            if ((numberArray[0].word.toUpperCase().charAt(0) === ("E") || ("U"))) {
                 console.log(`This means that you have until ${numberArray[1].word} to consume. That is it`)
             }
             else if ((numberArray[0].word.toUpperCase().charAt(0) === ("B"))) {
                 console.log(`This means that you have until ${numberArray[1].word} to consume while it is closed, however if stored properly, and quality looks suitbale, it make extend over the date`)
             }
+
+            else if ((numberArray[0].word.toUpperCase().charAt(0) === ("B" || "D")) && (numberArray[2].word.toUpperCase().charAt(0) === ("E") || ("U"))) {
+                console.log(`This means that you have until ${numberArray[1].word} to consume while it is closed, however if stored properly, and quality looks suitbale, it make extend over the date`)
+                console.log(`This means that you have until ${numberArray[3].word} to consume. That is it`)
+            }
+
 
         } catch (error) {
             console.error("Can't send the file", error);
