8d5841d9afe1f89cf4bc150156c97aad1f6faf11
MrSamerM <2106040@brunel.ac.uk>
2025-02-26 21:42:24 +0000

    started to test signup

--- src/testing/SignUp.test.js
+++ src/testing/SignUp.test.js
@@ -1,21 +1,111 @@
 import Signup from "../pages/SignUp";
 import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import axios from "axios";
-import { MemoryRouter } from "react-router-dom";  // Import MemoryRouter
+import { MemoryRouter } from "react-router-dom";
+
+// learnt how to use testing library by experimenting with chatgpt, and learning different methods
 
 
 jest.mock("axios");
 
-describe("Form Component", () => {
-    test("shows error if fields are empty", () => {
+// describe("Sign up form missing fields", () => {
+
+//     test("shows error if all fields are empty", () => {
+//         render(
+//             <MemoryRouter>
+//                 <Signup setSessionAvailability={jest.fn()} />
+//             </MemoryRouter>);
+
+//         fireEvent.click(screen.getByTestId("signup"));
+
+//         expect(screen.getByTestId("validateMessage").textContent).toBe("Must input all details");
+//     });
+
+
+
+//     test("shows error if name field is empty", () => {
+//         render(
+//             <MemoryRouter>
+//                 <Signup setSessionAvailability={jest.fn()} />
+//             </MemoryRouter>);
+
+//         const name = screen.getByTestId('name');;
+//         const surname = screen.getByTestId('surname');
+//         const dateOfBirth = screen.getByTestId('dateOfBirth');
+//         const email = screen.getByTestId('email');
+//         const password = screen.getByTestId('password');
+//         const signup = screen.getByTestId('signup');
+//         const validateMessage = screen.getByTestId("validateMessage");
+
+
+//         fireEvent.change(name, { target: { value: '' } });
+//         fireEvent.change(surname, { target: { value: 'hhey' } });
+//         fireEvent.change(dateOfBirth, { target: { value: '2002-09-10' } });
+//         fireEvent.change(email, { target: { value: 'sam@gmail.com' } });
+//         fireEvent.change(password, { target: { value: 'password' } });
+
+
+//         fireEvent.click(signup);
+
+//         expect(validateMessage.textContent).toBe("Must input all details");
+//     });
+
+
+//     test("shows error if email field is empty", () => {
+//         render(
+//             <MemoryRouter>
+//                 <Signup setSessionAvailability={jest.fn()} />
+//             </MemoryRouter>);
+
+//         const name = screen.getByTestId('name');;
+//         const surname = screen.getByTestId('surname');
+//         const dateOfBirth = screen.getByTestId('dateOfBirth');
+//         const email = screen.getByTestId('email');
+//         const password = screen.getByTestId('password');
+//         const signup = screen.getByTestId('signup');
+//         const validateMessage = screen.getByTestId("validateMessage");
+
+
+//         fireEvent.change(name, { target: { value: 'Sam' } });
+//         fireEvent.change(surname, { target: { value: 'hhey' } });
+//         fireEvent.change(dateOfBirth, { target: { value: '2002-09-10' } });
+//         fireEvent.change(email, { target: { value: '' } });
+//         fireEvent.change(password, { target: { value: 'password' } });
+
+//         fireEvent.click(signup);
+
+//         expect(validateMessage.textContent).toBe("Must input all details");
+//     });
+// }
+// );
+
+
+describe("password less than or equal to 6", () => {
+    test("shows error if password is not of length <= 6", async () => {
         render(
             <MemoryRouter>
                 <Signup setSessionAvailability={jest.fn()} />
-            </MemoryRouter>);
+            </MemoryRouter>
+        );
 
-        fireEvent.click(screen.getByTestId("signup"));
+        const name = screen.getByTestId('name');;
+        const surname = screen.getByTestId('surname');
+        const dateOfBirth = screen.getByTestId('dateOfBirth');
+        const email = screen.getByTestId('email');
+        const password = screen.getByTestId('password');
+        const signup = screen.getByTestId('signup');
 
-        expect(screen.getByTestId("validateMessage").textContent).toBe("Must input all details");
+
+        fireEvent.change(name, { target: { value: 'Sam' } });
+        fireEvent.change(surname, { target: { value: 'hhey' } });
+        fireEvent.change(dateOfBirth, { target: { value: '2002-09-10' } });
+        fireEvent.change(email, { target: { value: 'sam@gmail.com' } });
+        fireEvent.change(password, { target: { value: 'pass' } });
+
+        fireEvent.click(signup);
+
+        await waitFor(() => {
+            expect(screen.getByTestId("validateMessage").textContent).toBe("Must have a longer password");
+        });
     });
-}
-)
+});
