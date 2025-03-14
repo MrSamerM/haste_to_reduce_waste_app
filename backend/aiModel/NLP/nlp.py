e9b42abcca267fc738767530c84aca3594846496
MrSamerM <2106040@brunel.ac.uk>
2025-03-14 14:39:30 +0000

    improved accuracy of the nlp by adding a confidence level

--- backend/aiModel/NLP/nlp.py
+++ backend/aiModel/NLP/nlp.py
@@ -27,6 +27,8 @@ customData = pd.read_csv(dataset_path)
 question_tokens=[]
 response_tokens=[]
 stop_words=set(stopwords.words('english'))
+stop_words=[word for word in stop_words if word not in("where","you")]
+
 
 for question in customData['Questions']:
 
@@ -62,18 +64,23 @@ def genResponse(input):
     preprocessed_input = process_text(input)
     input_vector = vectorizer.transform([preprocessed_input])
 
-    # 5 lines below received by chatGPT. where cosin similarity is used to find the index with the similar questions. 03/03/2025
+    # 3 lines below, and two at the end received by chatGPT. where cosin similarity is used to find the index with the similar questions. 03/03/2025
     # Prompt: the response I get depends on the label. But how about if I want accurate results. For example the difference between saying hi and hello is a different response. Both with the same label
   
     question_vectors = vectorizer.transform(customData['Question_Token'])
     similarities = cosine_similarity(input_vector, question_vectors).flatten() #compares input with all questions in dataset. .flatten changes dataset to 1d
     best_match_idx = np.argmax(similarities) #find the index with the highest similarity
+    
+    confidence_level=max(similarities)
+    if confidence_level<0.85:
+        return "I am sorry you have to be more specific"
+
     best_response = customData.iloc[best_match_idx]['Response'] #the response is the row with the best closest index, from response column
     return best_response
 
-    
-# while True:
-#     user_input = input("You: ")
-#     if user_input.lower() == "exit":
-#         break
-#     print("Bot:", genResponse(user_input))
+
+while True:
+    user_input = input("You: ")
+    if user_input.lower() == "exit":
+        break
+    print("Bot:", genResponse(user_input))
